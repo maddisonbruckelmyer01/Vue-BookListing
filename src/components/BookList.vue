83b1015376941beecee563cef0ec1fc509472c26
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-08-05 18:15:39 -0500

    adds booklist

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1> {{title}}</h1>
+  </div>
+</template>
+
+<script> 
+  export default(
+  name = "BookList";
+  new data(){
+    return title = "All Books";
+  };
+  );
+</script>
+
+<style>
+  h1{
+    font-weight: normal;
+  }
+  h2{
+    font-weight: normal;
+  }
+</style>
