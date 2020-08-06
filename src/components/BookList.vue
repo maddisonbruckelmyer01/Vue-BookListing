4e7f8e274ca3fde7a0564d84d0fe00b742c2f4bf
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-08-06 11:07:55 -0500

    adds list items and array of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1> {{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,7 +13,8 @@
     data() {
       return {
         title: 'All Books'
-      }
+      },
+      books[ { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' }, { title: 'American Gods', author: 'Neil Gaiman' }, { title: 'Amusing Ourselves to Death', author: 'Neil Postman' }]
     }
   }
 </script>
@@ -19,5 +23,12 @@
   h1, h2{
     font-weight: normal;
   }
-
+  ul{
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: block;
+    margin: 0 10px;
+  }
 </style>
